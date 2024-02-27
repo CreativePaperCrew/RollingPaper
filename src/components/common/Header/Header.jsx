1c00ceaaffa894aaa95a890a4dd4608a42a67e93
yeona <aya813@naver.com>
2024-02-28 00:29:59 +0900

    feat: Header 컴포넌트 로고 클릭 시 Link를 통해 이동

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import {
   HeaderButton,
   HeaderContent,
@@ -11,10 +12,12 @@ const Header = () => {
   return (
     <HeaderContentContainer>
       <HeaderContent>
-        <LogoContainer>
-          <img src={LogoImg} alt="로고 이미지" />
-          <LogoContent>Rolling</LogoContent>
-        </LogoContainer>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <LogoContainer>
+            <img src={LogoImg} alt="로고 이미지" />
+            <LogoContent>Rolling</LogoContent>
+          </LogoContainer>
+        </Link>
         <HeaderButton>롤링 페이퍼 만들기</HeaderButton>
       </HeaderContent>
     </HeaderContentContainer>

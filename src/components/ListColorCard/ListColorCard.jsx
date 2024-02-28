ca7d3e487f7d65f989ba486aaad3129380b8cc47
yeona <aya813@naver.com>
2024-02-28 15:17:11 +0900

    feat: ListColorCard 컴포넌트 생성

---
+++ src/components/ListColorCard/ListColorCard.jsx
@@ -0,0 +1,61 @@
+import {
+  MessageCount,
+  MessageCountContainer,
+  MessageCountText,
+  Recipient,
+  RecipientContainer,
+  Divider,
+  EmojiContainer,
+  CardContentContainer,
+  CardContainer,
+  CardPattern,
+} from './ListColorCardStyle';
+import PatternImgPurple from '../../assets/images/CardPattern1.svg';
+import PatternImgBeige from '../../assets/images/CardPattern2.svg';
+import PatternImgBlue from '../../assets/images/CardPattern3.svg';
+import PatternImgGreen from '../../assets/images/CardPattern4.svg';
+
+const ListColorCard = () => {
+  const backgroundColor = 'purple';
+  let patternColor;
+
+  switch (backgroundColor) {
+    case 'beige':
+      patternColor = PatternImgBeige;
+      break;
+    case 'purple':
+      patternColor = PatternImgPurple;
+      break;
+    case 'blue':
+      patternColor = PatternImgBlue;
+      break;
+    case 'green':
+      patternColor = PatternImgGreen;
+      break;
+    default:
+      patternColor = PatternImgPurple;
+      break;
+  }
+
+  return (
+    <CardContainer color={backgroundColor}>
+      <CardContentContainer>
+        <RecipientContainer>
+          <Recipient>To. Sowon</Recipient>
+          <p>추가예정</p>
+          <MessageCountContainer>
+            <MessageCount>30</MessageCount>
+            <MessageCountText>명이 작성했어요</MessageCountText>
+          </MessageCountContainer>
+        </RecipientContainer>
+        <EmojiContainer>
+          <Divider />
+          <div>추가예정</div>
+        </EmojiContainer>
+      </CardContentContainer>
+      <CardPattern src={patternColor} alt="패턴이미지" />
+    </CardContainer>
+  );
+};
+
+export default ListColorCard;
---
+++ src/components/ListColorCard/ListColorCardStyle.js
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+
+const getBackgroundColor = (color) => {
+  switch (color) {
+    case 'beige':
+      return 'var(--orange-200)';
+    case 'purple':
+      return 'var(--purple-200)';
+    case 'blue':
+      return 'var(--blue-200)';
+    case 'green':
+      return 'var(--green-200)';
+    default:
+      return 'var(--purple-200)';
+  }
+};
+
+export const CardContainer = styled.div`
+  position: relative;
+  width: 17.1875rem;
+  height: 16.25rem;
+  flex-shrink: 0;
+  border-radius: 1rem;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  background: ${(props) => getBackgroundColor(props.color)};
+  box-shadow: 0 0.125rem 0.75rem 0 rgba(0, 0, 0, 0.08);
+  padding: 1.875rem 1.5rem 1.25rem;
+`;
+
+export const CardContentContainer = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 2.6875rem;
+`;
+
+export const RecipientContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.75rem;
+`;
+
+export const Recipient = styled.h2`
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  align-self: stretch;
+  overflow: hidden;
+  color: var(--gray-900);
+  text-overflow: ellipsis;
+  font-family: Pretendard;
+  font-size: var(--font-24);
+  font-style: normal;
+  font-weight: var(--font-bold);
+  line-height: 2.25rem;
+  letter-spacing: -0.015rem;
+`;
+
+export const MessageCountContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
+
+export const MessageCount = styled.span`
+  color: var(--gray-700);
+  font-family: Pretendard;
+  font-size: var(--font-16);
+  font-style: normal;
+  font-weight: var(--font-bold);
+  line-height: 1.625rem;
+  letter-spacing: -0.01rem;
+`;
+
+export const MessageCountText = styled.p`
+  color: var(--gray-700);
+  font-family: Pretendard;
+  font-size: var(--font-16);
+  font-style: normal;
+  font-weight: var(--font-regular);
+  line-height: 1.625rem;
+  letter-spacing: -0.01rem;
+`;
+
+export const EmojiContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+`;
+
+export const Divider = styled.div`
+  width: 14.1875rem;
+  height: 0.0625rem;
+  background: rgba(0, 0, 0, 0.12);
+  z-index: 10;
+`;
+
+export const CardPattern = styled.img`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;

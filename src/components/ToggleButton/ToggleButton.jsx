864f05ae8ef74f686493029359dd85836f7c85f4
namju <skawn1228@gmail.com>
2024-02-28 21:57:32 +0900

    feat: 컬러 또는 이미지 선택 기능 추가
    - 컬러 또는 이미지 버튼 클릭하였을 때, 선택 효과 표시
    - 아직 기능 구현까지는 되지 않았음.

--- src/components/ToggleButton/ToggleButton.jsx
+++ src/components/ToggleButton/ToggleButton.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from 'react';
 import * as S from './ToggleButtonStyle';
 
 const ToggleButton = () => {
+  const [isColorSelected, setIsColorSelected] = useState(false);
+  const [isImageSelected, setIsImageSelected] = useState(false);
+
+  const handleColorClick = () => {
+    setIsColorSelected(!isColorSelected);
+    setIsImageSelected(false);
+  };
+
+  const handleImageClick = () => {
+    setIsColorSelected(false);
+    setIsImageSelected(!isImageSelected);
+  };
+
   return (
     <S.ButtonContainer>
-      <S.Button>컬러</S.Button>
-      <S.Button>이미지</S.Button>
+      {console.log(`color : ${isColorSelected}`)}
+      {console.log(`image : ${isImageSelected}`)}
+      <S.Button
+        onClick={handleColorClick}
+        className={isColorSelected && 'clicked'}
+      >
+        컬러
+      </S.Button>
+      <S.Button
+        onClick={handleImageClick}
+        className={isImageSelected && 'clicked'}
+      >
+        이미지
+      </S.Button>
     </S.ButtonContainer>
   );
 };

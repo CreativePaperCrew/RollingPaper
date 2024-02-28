5e75d064d639c7fbb16a689a003bcdfb771ff5a7
Trophy198 <spsp8426@naver.com>
2024-02-28 13:23:59 +0900

    refactor : 각 불러오는 API 변수명과 상수설정이 잘못되었던점 수정

--- src/apis/apiConfig.js
+++ src/apis/apiConfig.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const TEAM_BASE_URL = process.env.REACT_APP_BASE_API_URL;
+const BASE_URL = process.env.REACT_APP_BASE_API_URL;
 
 const apiClient = axios.create({
-  baseURL: TEAM_BASE_URL,
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
--- src/apis/teamApiConfig.js
+++ src/apis/teamApiConfig.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_TEAM_BASE_API_URL;
+const TEAM_BASE_URL = process.env.REACT_APP_TEAM_BASE_API_URL;
 
 const teamApiClient = axios.create({
-  baseURL: `${BASE_URL}`,
+  baseURL: TEAM_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },

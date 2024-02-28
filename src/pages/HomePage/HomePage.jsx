b1968438d1fa12a1ae115b756a1140cb4fe468fe
yeona <aya813@naver.com>
2024-02-28 11:40:29 +0900

    feat: Main 삭제 및 HomePage로 이동

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import Main from '../../components/Main/Main';
+import * as S from './HomepageStyle';
+import Header from '../../components/common/Header/Header';
+import MainContent from '../../components/MainContent/MainContent';
+import Button from '../../components/common/Button/Button';
 
 const HomePage = () => {
-  return <Main />;
+  return (
+    <>
+      <Header />
+      <S.MainContentContainer>
+        <MainContent />
+      </S.MainContentContainer>
+      <S.ButtonContainer>
+        <Button text="구경해보기" to="/list" width={17.5} />
+      </S.ButtonContainer>
+    </>
+  );
 };
 
 export default HomePage;

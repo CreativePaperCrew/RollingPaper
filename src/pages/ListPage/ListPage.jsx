3e6757759b4cac56b6cfa44ab01b18343e509dff
yeona <aya813@naver.com>
2024-02-28 20:23:21 +0900

    feat: API 호출 후 ListCard 렌더링하기

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -1,19 +1,25 @@
 import * as S from './ListPageStyle';
+import teamApiClient from '../../apis/teamApiConfig';
 import Button from '../../components/common/Button/Button';
 import Header from '../../components/common/Header/Header';
 import ListCard from '../../components/ListCard/ListCard';
+import { useEffect, useState } from 'react';
 
 const ListPage = () => {
-  const dummyData = {
-    name: '안연아',
-    messageCount: 5,
-    backgroundColor: 'beige',
-    backgroundImageURL: 'https://picsum.photos/id/683/3840/2160',
-  };
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    teamApiClient.get('/recipients/').then((res) => {
+      setListData(res.data.results);
+    });
+  }, []);
+
   return (
     <>
       <Header />
-      <ListCard cardData={dummyData} />
+      {listData.map((element) => (
+        <ListCard key={element.id} cardData={element} />
+      ))}
       <S.ButtonContainer>
         <Button text="나도 만들어보기" to="/post" width={17.5} />
       </S.ButtonContainer>

89237d895bf7f249628125d9f407c4e55820dbd7
yeona <aya813@naver.com>
2024-02-28 15:40:21 +0900

    refactor: StyledComponent import 시 * as S로 변경 후 적용

--- src/components/ListColorCard/ListColorCard.jsx
+++ src/components/ListColorCard/ListColorCard.jsx
@@ -1,15 +1,4 @@
-import {
-  MessageCount,
-  MessageCountContainer,
-  MessageCountText,
-  Recipient,
-  RecipientContainer,
-  Divider,
-  EmojiContainer,
-  CardContentContainer,
-  CardContainer,
-  CardPattern,
-} from './ListColorCardStyle';
+import * as S from './ListColorCardStyle';
 import PatternImgPurple from '../../assets/images/CardPattern1.svg';
 import PatternImgBeige from '../../assets/images/CardPattern2.svg';
 import PatternImgBlue from '../../assets/images/CardPattern3.svg';
@@ -38,23 +27,23 @@ const ListColorCard = ({ cardData }) => {
   }
 
   return (
-    <CardContainer color={backgroundColor}>
-      <CardContentContainer>
-        <RecipientContainer>
-          <Recipient>To. {name}</Recipient>
+    <S.CardContainer color={backgroundColor}>
+      <S.CardContentContainer>
+        <S.RecipientContainer>
+          <S.Recipient>To. {name}</S.Recipient>
           <p>추가예정</p>
-          <MessageCountContainer>
-            <MessageCount>{messageCount}</MessageCount>
-            <MessageCountText>명이 작성했어요</MessageCountText>
-          </MessageCountContainer>
-        </RecipientContainer>
-        <EmojiContainer>
-          <Divider />
+          <S.MessageCountContainer>
+            <S.MessageCount>{messageCount}</S.MessageCount>
+            <S.MessageCountText>명이 작성했어요</S.MessageCountText>
+          </S.MessageCountContainer>
+        </S.RecipientContainer>
+        <S.EmojiContainer>
+          <S.Divider />
           <div>추가예정</div>
-        </EmojiContainer>
-      </CardContentContainer>
-      <CardPattern src={patternColor} alt="패턴이미지" />
-    </CardContainer>
+        </S.EmojiContainer>
+      </S.CardContentContainer>
+      <S.CardPattern src={patternColor} alt="패턴이미지" />
+    </S.CardContainer>
   );
 };
 

7d35d649f4bf07d6326d409f22c2a5c574c13e71
JaeBeen95 <nin2348@gmail.com>
2024-02-29 14:08:05 +0900

    refactor: px로 적용된 스타일을 rem으로 수정, 기본 weight가 400이므로, font-weight삭제

--- src/components/Badge/BadgeStyle.js
+++ src/components/Badge/BadgeStyle.js
@@ -7,14 +7,13 @@ export const BadgeContainer = styled.div`
   gap: 0.625rem;
   padding: 0px 0.5rem;
   background: ${({ background }) => background};
-  border-radius: 4px;
+  border-radius: 0.25rem;
 `;
 
 export const Relationship = styled.p`
   color: ${({ color }) => color};
   font-size: var(--font-14);
   font-style: var(--font-regular);
-  font-weight: 400;
   line-height: 1.25rem;
   letter-spacing: -0.0044rem;
 `;

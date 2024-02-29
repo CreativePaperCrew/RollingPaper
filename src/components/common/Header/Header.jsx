be278419027c2b6bfe6dd7e7f8dac425d1dfbdac
yeona <aya813@naver.com>
2024-02-29 14:09:16 +0900

    feat: Header Router에서 고정 레이아웃으로 구현

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,7 +1,14 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import * as S from './HeaderStyle';
 import LogoImg from '../../../assets/icons/icon.svg';
 
 const Header = () => {
+  const location = useLocation();
+
+  const shouldShowButton =
+    location.pathname === '/' || location.pathname === '/list';
+
   return (
     <S.HeaderContentContainer>
       <S.HeaderContent>
@@ -11,10 +18,11 @@ const Header = () => {
             <S.LogoContent>Rolling</S.LogoContent>
           </S.LogoContainer>
         </S.ButtonLink>
-
-        <S.ButtonLink to="/post">
-          <S.HeaderButton>롤링 페이퍼 만들기</S.HeaderButton>
-        </S.ButtonLink>
+        {shouldShowButton && (
+          <S.ButtonLink to="/post">
+            <S.HeaderButton>롤링 페이퍼 만들기</S.HeaderButton>
+          </S.ButtonLink>
+        )}
       </S.HeaderContent>
     </S.HeaderContentContainer>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import * as S from './HomepageStyle';
-import Header from '../../components/common/Header/Header';
 import MainContent from '../../components/MainContent/MainContent';
 import LinkButton from '../../components/common/LinkButton/LinkButton';
 
 const HomePage = () => {
   return (
     <>
-      <Header />
       <S.MainContentContainer>
         <MainContent />
       </S.MainContentContainer>

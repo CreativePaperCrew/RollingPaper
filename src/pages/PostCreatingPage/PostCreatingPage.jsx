539f8571a0de271e4f454c0bc4535eff02500937
namju <skawn1228@gmail.com>
2024-02-29 22:28:26 +0900

    refactor: 불필요한 코드 삭제
    - PostCreatingPage.jsx 내의 dead code 제거

--- src/pages/PostCreatingPage/PostCreatingPage.jsx
+++ src/pages/PostCreatingPage/PostCreatingPage.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
 import * as S from './PostCreatingPageStyle';
 import Input from '../../components/Input/Input';
 import BackgroundTypeSelectButton from '../../components/BackgroundTypeSelectButton/BackgroundTypeSelectButton';
@@ -18,28 +18,25 @@ const PostCreatingPage = () => {
 
   return (
     <>
-      <header></header>
-      <main>
-        <S.ContainerWhole>
-          <S.ContainerReceiver>
-            <S.Title>To.</S.Title>
-            <Input width="long">받는 사람 이름을 입력해주세요.</Input>
-          </S.ContainerReceiver>
-          <S.ContainerSelect>
-            <S.Title>배경화면을 선택해 주세요.</S.Title>
-            <S.InformationMessage>
-              컬러를 선택하거나, 이미지를 선택할 수 있습니다.
-            </S.InformationMessage>
-          </S.ContainerSelect>
-          <BackgroundTypeSelectButton
-            handleColorClick={handleColorClick}
-            handleImageClick={handleImageClick}
-            select={select}
-          />
-          <BackgroundSelector select={select} />
-          <LinkButton width="45" text="생성하기" />
-        </S.ContainerWhole>
-      </main>
+      <S.ContainerWhole>
+        <S.ContainerReceiver>
+          <S.Title>To.</S.Title>
+          <Input width="long">받는 사람 이름을 입력해주세요.</Input>
+        </S.ContainerReceiver>
+        <S.ContainerSelect>
+          <S.Title>배경화면을 선택해 주세요.</S.Title>
+          <S.InformationMessage>
+            컬러를 선택하거나, 이미지를 선택할 수 있습니다.
+          </S.InformationMessage>
+        </S.ContainerSelect>
+        <BackgroundTypeSelectButton
+          handleColorClick={handleColorClick}
+          handleImageClick={handleImageClick}
+          select={select}
+        />
+        <BackgroundSelector select={select} />
+        <LinkButton width="45" text="생성하기" />
+      </S.ContainerWhole>
     </>
   );
 };

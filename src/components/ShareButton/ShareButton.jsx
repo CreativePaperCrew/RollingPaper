ca1c094c749acecc6bb4e750d74121e908930757
Trophy198 <spsp8426@naver.com>
2024-02-29 19:31:07 +0900

    feat: ShareButton 컴포넌트에 Toast URL 클립보드 저장 초기 기능 추가

--- src/components/ShareButton/ShareButton.jsx
+++ src/components/ShareButton/ShareButton.jsx
@@ -1,4 +1,5 @@
 import useToggle from '../../hooks/useToggle';
+import Toast from '../common/Toast/Toast';
 import shareIconSvg from '../../assets/icons/share.svg';
 import {
   ShareButtonContainer,
@@ -6,8 +7,31 @@ import {
   ShareIcon,
   ShareOption,
 } from './ShareButtonStyle';
+import useToast from '../../hooks/useToast';
+
 const ShareButton = () => {
+  const {
+    showToast,
+    isShow,
+    isShouldRender,
+    isSuccess,
+    message,
+    startHidingToast,
+  } = useToast();
   const [isToggleOpen, changeToggle] = useToggle();
+
+  const hostAddress = window.location.origin;
+  const shareLink = `${hostAddress}/post/id`;
+
+  const handleShare = async () => {
+    try {
+      await navigator.clipboard.writeText(shareLink);
+      showToast(true, 'URL이 클립보드에 복사되었습니다!');
+    } catch (error) {
+      showToast(false, 'URL 복사에 실패하였습니다.');
+    }
+  };
+
   return (
     <ShareButtonContainer onClick={changeToggle}>
       <ShareIcon src={shareIconSvg} alt="share icon" />
@@ -16,8 +40,17 @@ const ShareButton = () => {
           <ShareOption onClick={() => alert('카톡공유')}>
             카카오톡 공유
           </ShareOption>
-          <ShareOption onClick={() => alert('링크공유')}>URL 공유</ShareOption>
+          <ShareOption onClick={() => handleShare()}>URL 공유</ShareOption>
         </ShareDropdown>
+      )}
+      {isShouldRender && (
+        <Toast
+          isSuccess={isSuccess}
+          message={message}
+          isShow={isShow}
+          isShouldRender={isShouldRender}
+          startHidingToast={startHidingToast}
+        />
       )}
     </ShareButtonContainer>
   );

b413adb1c0dc3fc6abbe642202fb2d1d6c0bade9
JaeBeen95 <nin2348@gmail.com>
2024-02-29 20:26:06 +0900

    refactor: PostCard와 ServiceHeader과의 간격 적용

--- src/pages/PostPage/PostPageStyle.js
+++ src/pages/PostPage/PostPageStyle.js
@@ -8,19 +8,19 @@ export const PostCardsContainer = styled.div`
   gap: 1rem;
   align-items: center;
   height: 100vh;
-  padding: 0 1.25rem 0;
+  padding: 2rem 1.25rem;
   overflow: scroll;
   scrollbar-width: none;
 
   @media (min-width: 768px) {
     grid-template-columns: repeat(2, 22.125rem);
     grid-template-rows: repeat(auto-fit, 17.75rem);
-    padding: 0 1.5rem;
+    padding: 5.8125rem 1.5rem;
   }
 
   @media (min-width: 1248px) {
     grid-template-columns: repeat(3, 24rem);
     grid-template-rows: repeat(auto-fit, 17.5rem);
-    padding: 0;
+    padding: 7.0625rem 0;
   }
 `;

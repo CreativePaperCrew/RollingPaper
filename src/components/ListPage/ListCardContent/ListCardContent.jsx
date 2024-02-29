44092551da2489b482fa69098b6e284d94f25178
yeona <aya813@naver.com>
2024-02-29 12:17:58 +0900

    feat: 생성된 컴포넌트 클릭 시 해당 post/{id}로 이동

--- src/components/ListPage/ListCardContent/ListCardContent.jsx
+++ src/components/ListPage/ListCardContent/ListCardContent.jsx
@@ -1,6 +1,7 @@
 import ListCard from '../ListCard/ListCard';
 import { LeftArrowButton, RigthArrowButton } from '../ArrowButton/ArrowButton';
 import * as S from './ListCardContentStyle';
+import { Link } from 'react-router-dom';
 
 const ListCardContent = ({
   sortedDataList,
@@ -11,7 +12,9 @@ const ListCardContent = ({
   return (
     <S.ListCardsContainer>
       {sortedDataList.slice(slideIndex, slideIndex + 4).map((element) => (
-        <ListCard key={element.id} cardData={element} />
+        <Link to={`/post/${element.id}`} key={element.id}>
+          <ListCard key={element.id} cardData={element} />
+        </Link>
       ))}
       {slideIndex > 0 && (
         <S.LeftArrowButtonContainer>

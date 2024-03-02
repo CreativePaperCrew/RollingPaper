171e78e516cd5a54491e9ec1b13d7afcc5d966c0
namju <skawn1228@gmail.com>
2024-03-02 15:44:28 +0900

    fix: 없는 페이지 삭제 및 colors.js import 방식 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import PostCreatingPage from './pages/PostCreatingPage/PostCreatingPage';
-
 import AppLayout from './AppLayout';
 import RecipientsPage from './pages/RecipientsPage/RecipientsPage';
 

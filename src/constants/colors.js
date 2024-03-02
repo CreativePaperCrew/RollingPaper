fa207dd52351112ebeacef3f36018d5a3759abfc
yeona <aya813@naver.com>
2024-03-02 16:15:12 +0900

    fix: colors.js export 수정

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,8 +1,6 @@
-const COLORS = Object.freeze({
+export const COLORS = Object.freeze({
   beige: '--orange-200',
   purple: '--purple-200',
   blue: '--blue-200',
   green: '--green-200',
 });
-
-export default COLORS;

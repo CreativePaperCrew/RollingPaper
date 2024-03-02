da63116f281fdcbcbb2fd16dc91144bb51cb4dca
Trophy198 <spsp8426@naver.com>
2024-03-02 11:26:12 +0900

    fix: 병합 충돌 해결 후 prop 에서 값이 넘어오지 않는 현상 수정
    - Stlyle -> Style 네이밍 변경

--- src/components/ServiceHeader/ServiceHeader.jsx
+++ src/components/ServiceHeader/ServiceHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import EmojiPicker from '../../components/EmojiPicker/EmojiPicker';
-import * as S from './ServiceHeaderStlyle';
+import * as S from './ServiceHeaderStyle';
 import ShareButton from '../ShareButton/ShareButton';
 import TopReactions from '../TopReactions/TopReactions';
 import ReactionDropdown from '../ReactionDropdown/ReactionDropdown';
@@ -12,14 +12,14 @@ const ServiceHeader = ({ recipientData }) => {
         <S.HorizontalBar />
         <S.ServiceHeaderContainer>
           <S.NameArea>To. {recipientData.name}</S.NameArea>
-          <S.HorizontalBar $$onlyMobile={true} />
+          <S.HorizontalBar $onlyMobile={true} />
           <S.OptionArea>
-            <S.VerticalBar $$margin="1.75rem" $$onlyMobile={false} />
+            <S.VerticalBar $margin="1.75rem" $onlyMobile={false} />
             <TopReactions topReactions={recipientData.topReactions} />
             <ReactionDropdown />
             <EmojiPicker />
-            <S.VerticalBar $$margin="0.8125rem" $$marginonlyMobile={false} />
-            <S.VerticalBar $$margin="0.9375rem;" $$onlyMobile={true} />
+            <S.VerticalBar $margin="0.8125rem" $onlyMobile={false} />
+            <S.VerticalBar $margin="0.9375rem;" $onlyMobile={true} />
             <ShareButton />
           </S.OptionArea>
         </S.ServiceHeaderContainer>

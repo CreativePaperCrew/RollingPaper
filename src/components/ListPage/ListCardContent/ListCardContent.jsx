0485f6c786142ba9d074678b17ccc92e4919a223
yeona <aya813@naver.com>
2024-03-04 14:39:16 +0900

    refactor: 커스텀 훅으로 useFetchRecipient 분리

--- src/components/ListPage/ListCardContent/ListCardContent.jsx
+++ src/components/ListPage/ListCardContent/ListCardContent.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { getRecipients } from '../../../apis/recipientRollingPaperApi';
+import useRecipientData from '../../../hooks/useFetchRecipient';
 import {
   LeftArrowButton,
   RightArrowButton,
@@ -8,10 +8,22 @@ import ListCard from '../ListCard/ListCard';
 import * as S from './ListCardContentStyle';
 
 const ListCardContent = ({ isSortLike }) => {
-  const [listData, setListData] = useState([]);
   const [isScrollable, setIsScrollable] = useState(false);
   const [limit, setLimit] = useState(4);
   const [offset, setOffset] = useState(0);
+
+  const {
+    data: listData,
+    isLoading,
+    error,
+  } = useRecipientData(limit, offset, isSortLike);
+
+  useEffect(() => {
+    updateScroll();
+    window.addEventListener('resize', updateScroll);
+
+    return () => window.removeEventListener('resize', updateScroll);
+  }, []);
 
   const updateScroll = () => {
     const isSmallScreen = window.innerWidth < 1200;
@@ -24,29 +36,6 @@ const ListCardContent = ({ isSortLike }) => {
     }
   };
 
-  useEffect(() => {
-    updateScroll();
-    window.addEventListener('resize', updateScroll);
-
-    return () => window.removeEventListener('resize', updateScroll);
-  }, []);
-
-  useEffect(() => {
-    const fetchRecipient = async () => {
-      try {
-        const response = await getRecipients({ limit, offset, isSortLike });
-        setListData(response);
-      } catch (error) {
-        console.error(
-          '롤링페이퍼 대상자를 불러오는데 실패했습니다:',
-          error.message,
-        );
-      }
-    };
-
-    fetchRecipient();
-  }, [limit, offset, isSortLike]);
-
   const handlePrevious = () => {
     setOffset(offset - 1);
   };
@@ -54,6 +43,14 @@ const ListCardContent = ({ isSortLike }) => {
   const handleNext = () => {
     setOffset(offset + 1);
   };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <S.ListCardsContainer $isScrollable={isScrollable}>

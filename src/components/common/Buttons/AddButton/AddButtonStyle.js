5f7d29ddb621eb78b1b4647c704e318283a56fe1
JaeBeen95 <nin2348@gmail.com>
2024-03-05 13:57:57 +0900

    refactor: AddButton에서 +버튼이 가운데에 있지 않은 현상 수정 / px을 rem으로 변경

--- src/components/common/Buttons/AddButton/AddButtonStyle.js
+++ src/components/common/Buttons/AddButton/AddButtonStyle.js
@@ -5,10 +5,13 @@ export const AddButtonContainer = styled.button`
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 3.5rem;
   height: 3.5rem;
   border: none;
-  border-radius: 100px;
+  border-radius: 6.25rem;
   background: var(--gray-500);
 
   &:hover {

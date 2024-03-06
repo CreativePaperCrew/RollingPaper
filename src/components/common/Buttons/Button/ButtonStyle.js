063b9045bf0e15f7439c576f813460c45ba71467
yeona <aya813@naver.com>
2024-03-06 11:24:40 +0900

    refactor: BUTTON_SIZE 상수를 버튼 컴포넌트에서 관리

--- src/components/common/Buttons/Button/ButtonStyle.js
+++ src/components/common/Buttons/Button/ButtonStyle.js
@@ -1,5 +1,31 @@
 import styled from 'styled-components';
-import { BUTTON_SIZE } from '../../../../constants/buttonSize';
+
+const BUTTON_SIZE = {
+  small: {
+    padding: '0.4375rem 1rem',
+    borderRadius: '0.375rem',
+    fontSize: 'var(--font-16)',
+    fontWeight: 'var(--font-regular)',
+    letterSpacing: '-0.01rem',
+    width: '5.75rem',
+  },
+  medium: {
+    padding: '0.875rem 1.5rem',
+    borderRadius: '0.75rem',
+    fontSize: 'var(--font-18)',
+    fontWeight: 'var(--font-bold)',
+    letterSpacing: '-0.0112rem',
+    width: '17.5rem',
+  },
+  large: {
+    padding: '0.875rem 1.5rem',
+    borderRadius: '0.75rem',
+    fontSize: 'var(--font-18)',
+    fontWeight: 'var(--font-bold)',
+    letterSpacing: '-0.0112rem',
+    width: '45rem',
+  },
+};
 
 export const Button = styled.button`
   display: flex;
@@ -16,7 +42,7 @@ export const Button = styled.button`
   font-size: ${(props) => BUTTON_SIZE[props.size].fontSize};
   font-weight: ${(props) => BUTTON_SIZE[props.size].fontWeight};
   letter-spacing: ${(props) => BUTTON_SIZE[props.size].letterSpacing};
-  width: ${(props) => BUTTON_SIZE[props.size].width};
+  width: ${(props) => (props.size ? BUTTON_SIZE[props.size].width : '100%')};
 
   &:hover {
     background: var(--purple-700);

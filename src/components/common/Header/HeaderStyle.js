fbe999f9378eb7999bfe750e75baceeed6e2ddca
JaeBeen95 <nin2348@gmail.com>
2024-03-07 11:17:12 +0900

    refactor: postcard padding-bottom값 수정

--- src/components/common/Header/HeaderStyle.js
+++ src/components/common/Header/HeaderStyle.js
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { VIEWPORT_SIZE } from '../../../constants/viewportSize';
 
 export const HeaderContentContainer = styled.div`
   display: flex;
